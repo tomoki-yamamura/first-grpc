32917c2e7050f2e874d39604559b63280ffac8c5
tomoki-yamamura <progtab17@gmail.com>
2022-12-01 20:32:19 +0900

    3

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,7 +4,9 @@ import (
 	// (一部抜粋)
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	hellopb "mygrpc/pkg/grpc"
 	"os"
@@ -43,7 +45,9 @@ func main() {
 
 	for {
 		fmt.Println("1: send Request")
-		fmt.Println("2: exit")
+		fmt.Println("2: HelloServerStream")
+		fmt.Println("3: HelloClientStream")
+		fmt.Println("4: exit")
 		fmt.Print("please enter >")
 
 		scanner.Scan()
@@ -52,8 +56,10 @@ func main() {
 		switch in {
 		case "1":
 			Hello()
-
 		case "2":
+			HelloServerStream()
+		case "3":
+		case "4":
 			fmt.Println("bye.")
 			goto M
 		}
@@ -70,6 +76,60 @@ func Hello() {
 		Name: name,
 	}
 	res, err := client.Hello(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloServerStream() {
+	fmt.Println("Please enter your name")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	stream, err := client.HelloServerStream(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		res, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("all the responses have already received.")
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
+	}
+}
+
+func HelloClientStream() {
+	stream, err := client.HelloClientStream(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	sendCount := 5
+	fmt.Printf("Please enter %d names.\n", sendCount)
+	for i := 0; i < sendCount; i++ {
+		scanner.Scan()
+		name := scanner.Text()
+
+		if err := stream.Send(&hellopb.HelloRequest{
+			Name: name,
+		}); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	res, err := stream.CloseAndRecv()
 	if err != nil {
 		fmt.Println(err)
 	} else {
